30c67895ca74cc7740434ec5b05feacc0de82ade
amimitaghosh99-alt <amimitaghosh99@gmail.com>
2026-08-02 07:37:17 +0530

    chore: cleanup script files

--- fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/App.tsx', 'utf-8');
-
-// I need to fetch the original lines from github!
-// Wait, I can just use git checkout to restore and then do it properly.
